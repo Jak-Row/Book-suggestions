2913725807113f6b0881fa741794aa87c4377a9b
Jacob Rowe <jrowe1970@att.net>
2024-08-06 19:21:16 -0400

    scrpits code

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const body = document.body;
+const container = document.querySelector(".container");
+const item1 = document.createElement('section');
+
+iteam1.textContent = "Harry Potter";
+
+container.appendChild(iteam1);
+
+console.log("hi");
