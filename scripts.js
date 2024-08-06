5d158f80050c0c039c07180bd76b3297449e5f9b
Jacob Rowe <jrowe1970@att.net>
2024-08-06 19:55:51 -0400

    changes

--- scripts.js
+++ scripts.js
@@ -1,9 +1,21 @@
 const body = document.body;
-const container = document.querySelector(".container");
+const container = document.querySelector(".book-item");
 const item1 = document.createElement('section');
+const item2 = document.createElement('section');
+const item3 = document.createElement('section');
 
-iteam1.textContent = "Harry Potter";
+const book = {
+    name: "The Sourcerer's Stone",
+    series: "Harry Potter",
+    genre: "Fantasy"
+}
 
-container.appendChild(iteam1);
+item1.textContent = `Title: ${book.name}`;
+item2.textContent = `Series: ${book.series}`;
+item3.textContent = `Genre: ${book.genre}`;
+
+container.appendChild(item1);
+container.appendChild(item2);
+container.appendChild(item3);
 
 console.log("hi");
