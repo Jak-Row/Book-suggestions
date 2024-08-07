2130734e997e0ae0d200e17f5e4c5540082fbad4
Heartless117 <christiancruz1997@gmail.com>
2024-08-06 19:02:07 -0500

    font change to Times new Roman

--- scripts.js
+++ scripts.js
@@ -2,8 +2,8 @@ const body = document.body;
 const container = document.querySelector(".container");
 const item1 = document.createElement('section');
 
-iteam1.textContent = "Harry Potter";
+item1.textContent = "Harry Potter";
 
-container.appendChild(iteam1);
+container.appendChild(item1);
 
 console.log("hi");
