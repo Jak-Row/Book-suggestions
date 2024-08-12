97879684d4c731b884829f5fb9c4f7c705a4e2a9
Jacob Rowe <jrowe1970@att.net>
2024-08-12 19:58:13 -0400

    added the add book button and display books fnction

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 const library = [{
     title: "The Sourcerer's Stone",
     series: "Harry Potter",
-    genre: "Fantasy"
+    genre: "Fantasy",
+    book: 1
 }];
 const addBook = document.querySelector('#newBook');
 const titleEl = document.getElementById("title");
@@ -12,33 +13,37 @@ const addNewBook = function(title, series, genre) {
     const newBook = {
         title: title,
         series: series,
-        genre: genre
+        genre: genre,
+        book: (library.length + 1)
     }
 
     library.push(newBook)
 }
 
 
-const desplayBook = function() {
+const displayBooks = function() {
 
     for(i = 0; i < library.length; i++) {
         const container = document.querySelector(".book-item");
         const item1 = document.createElement('h2');
-        const item2 = document.createElement('p');
+        const item2 = document.createElement('h2');
         const item3 = document.createElement('section');
+        const item4 = document.createElement('section');
 
         item1.textContent = `Title: ${library[i].title}`;
         item2.textContent = `Series: ${library[i].series}`;
         item3.textContent = `Genre: ${library[i].genre}`;
+        item4.textContent = `Book Number: ${library[i].book}`;
 
         container.appendChild(item1);
         container.appendChild(item2);
         container.appendChild(item3);
+        container.appendChild(item4);
     }
     
 }
 
-desplayBook();
+displayBooks();
 
 addBook.addEventListener('click', function () {
     const titleEl = document.getElementById("title").value;
